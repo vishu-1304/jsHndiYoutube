38ab6f68bfdb4123b6497f76cc6ac23432d80572
vishu-1304 <vishu.23b1531165@abes.ac.in>
2025-10-28 22:19:35 +0530

    jshindiyoutube

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmails = "vishu@google.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountstate;
+
+// accountId = 2
+accountEmails ="pratigya@gmail.com"
+accountPassword = "43536"
+accountCity =" bangaluru"
+
+console.log(accountId);
+
+/* prefer not to use var
+beacuse  of issue in block scope and functional scope  */
+console.table([accountId, accountEmails, accountPassword, accountCity, accountstate])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strick"; // treat all js code as newer version
+
+// alert( 3+ 3) // we are using node js, not browser
+console.log(3
+    +3)//code readiability should be high
+console.log("vishu")
+
+let name = "vishu"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power53
+// bright
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique 
+// objects
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);//object
+
