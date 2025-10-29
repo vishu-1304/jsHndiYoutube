62bc35746301af0f27f5078a651da3b371dbda7e
vishu-1304 <vishu.23b1531165@abes.ac.in>
2025-10-29 15:15:04 +0530

    comparison

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+let score ="33abs" 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33avd" => NaN
+
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn); 
+// 1 => true; 0 => false
+// vishu => true; "" => false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//**************operations ***************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+//console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "vishu"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+console.log (2> 1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+
+
+// console.log("2" > 1);
+//console.log("01" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null>= 0);
+
+
+console.log(undefined == 0);
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+
+ //in most of cases avoid this upper six line conparison
+
+ //===
+ console.log("2" === 2);
